1164aaf56f84b9d856d18de636662f7447fd947b
VastoLorde <ksenbonsakura@gmail.com>
2024-01-05 12:59:21 +0100

    Project Setup

---
+++ README.md
@@ -0,0 +1,5 @@
+# Pricesniffer, Modern Next.js Web scraping app
+
+## Tracking articles prices from different e-commerce platforms and propose the best prices for any article to save money on your online shopping
+
+Click here to visit app: [pricesniffer](www.google.com)
---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from 'next'
+import { Inter, Space_Grotesk } from 'next/font/google'
+import './globals.css'
+
+import { Navbar } from '@/components'
+
+const inter = Inter({ subsets: ['latin'] })
+const spaceGrotesk = Space_Grotesk({
+    subsets: ['latin'],
+    weight: ['300', '400', '500', '600', '700']
+})
+
+export const metadata: Metadata = {
+  title: 'Pricesniffer | Home',
+  description: 'Web scraping app for tracking articles prices from different e-commerce plateforms and propose the best prices for any article to save money on your online shopping.',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+        <body className={inter.className}>
+            <main className="max-w-10xl mx-auto">
+                <Navbar />
+                {children}
+            </main>
+        </body>
+    </html>
+  )
+}
